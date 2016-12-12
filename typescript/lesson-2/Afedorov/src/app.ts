29b3e267601cb210dc8b8b1b2c2e30b955e070fd
Alexey Fedorov <Alexey.Fedorov@confirmit.com>
2016-12-12 14:01:59 +0100

    Adding Task2 project build skeleton

---
+++ typescript/lesson-2/Afedorov/src/app.ts
@@ -0,0 +1,6 @@
+import 'whatwg-fetch'
+import * as _ from 'lodash'
+import './main.less'
+
+console.log('hello!')
+
---
+++ typescript/lesson-2/Afedorov/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {    
+    target: 'web',
+    
+    entry: {
+        main: ['./src/app.ts']
+        //css: ['./src/main.less']
+    },        
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].[hash].js',
+        publicPath: '/'
+    },
+
+    devtool: 'source-map',
+    debug: true,
+    postcss: () => [autoprefixer],
+
+    module: {
+        loaders: [
+            {
+                test: /\.(ts)$/,
+                loader: 'ts-loader',
+                include: path.join(__dirname, 'src'),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(css|less)$/,
+                 /*loaders: [
+                    'style',
+                    'css',
+                    'less',
+                    'postcss'
+                ]*/
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style',
+                    loader: 'css?sourceMap!less?resolve url!postcss'
+                })                
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),        
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+
+        new ExtractTextPlugin({filename: 'css/[name].css', allChunks: true})        
+    ],
+
+    resolve: {
+        //root: path.resolve('./src'),
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js', '.ts', '.less']        
+    },
+
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader', '*'],
+        extensions: ['', '.js']
+    },
+
+     externals: {
+        // Use external version of React
+        //"react": "React",
+    },
+
+    devServer: {
+        contentBase: './build',
+        port: 8080,
+        hot: true
+    }
+}
